b5141bc1314543ba14043d08fc20e89017c8de47
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-02-08 20:51:02 +0530

    pushed

--- ExchangeRate/s.js
+++ ExchangeRate/s.js
@@ -1,6 +1,10 @@
 let msg = document.getElementById("ratemsg");
 let swapbtn = document.getElementById("swapbtn");
 let inp;
+let input1
+let input2
+let currency1
+let currency2
 
 
 
@@ -11,14 +15,14 @@ async function Exchange(input,currency1,currency2) {
       `https://api.exchangerate-api.com/v4/latest/${currency1}`
     );
     response = await response.json();
-    console.log(response)
-    console.log(response.rates)
+   
+   
    
     let rate2 = response.rates[currency2];
    
     console.log(rate2)
     let result = input * (rate2)
-    
+    msg.innerHTML=`1${currency1}=${rate2} ${currency2}`
     return result
   } catch (e) {
     console.log("Error: " + e.message);
@@ -26,42 +30,46 @@ async function Exchange(input,currency1,currency2) {
 }
 async function fun1 () {
   inp=document.activeElement
-  let input1 = document.getElementById("input1").value;
+  input1 = document.getElementById("input1").value;
   
-  let currency1 = document.getElementById("currency-one").value.toUpperCase();
-  let currency2 = document.getElementById("currency-two").value.toUpperCase();
+  currency1 = document.getElementById("currency-one").value.toUpperCase();
+  currency2 = document.getElementById("currency-two").value.toUpperCase();
 
   
-  let result=await Exchange(input1,currency1,currency2)
+  result=await Exchange(input1,currency1,currency2)
   document.getElementById("input2").value=result
-  msg.innerHTML=`1${currency1}=${result} ${currency2}`
-
+ 
 }
 async function fun2(){
   inp=document.activeElement
-   let input2 = document.getElementById("input2").value;
+   input2 = document.getElementById("input2").value;
  
-   let currency1 = document.getElementById("currency-one").value.toUpperCase();
-   let currency2 = document.getElementById("currency-two").value.toUpperCase();
+   currency1 = document.getElementById("currency-one").value.toUpperCase();
+   currency2 = document.getElementById("currency-two").value.toUpperCase();
  
    
    let result=await Exchange(input2,currency2,currency1)
    document.getElementById("input1").value=result
-   msg.innerHTML=`1${currency1}=${result} ${currency2}`
+  
  }
-input1.addEventListener("input",fun1);
 
-input2.addEventListener("input",fun2);
+ document.getElementById("input1").addEventListener("input",fun1);
+
+ document.getElementById("input2").addEventListener("input",fun2);
 
 swapbtn.addEventListener("click",async()=>{
   
   console.log(inp.id)
   if(inp.id=='input1'){
+    document.getElementById('input2').value=inp.value
     fun2()
+    inp=document.getElementById('input2')
   
   }
   else{
+    document.getElementById('input1').value=inp.value
     fun1()
-
+    inp=document.getElementById('input1')
   }
+  
 })
